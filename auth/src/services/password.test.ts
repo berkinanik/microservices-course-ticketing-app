a13653fa72d19489b6c915405a85e72f8f4f0290
Berkin Anik <berkinanik@gmail.com>
2024-04-02 00:31:42 +0200

    feat(auth): add jest and setup tests

--- auth/src/services/password.test.ts
+++ auth/src/services/password.test.ts
@@ -1,23 +1,24 @@
-import { describe, it } from 'node:test';
-import assert from 'node:assert';
-
 import { Password } from './password';
 
-describe('Password service', async () => {
+describe('Password service', () => {
   const password = 'my-password';
-  const hashed = await Password.toHash(password);
+  let hashed: string;
+
+  beforeAll(async () => {
+    hashed = await Password.toHash(password);
+  });
 
   it('toHash returns hashed password with salt', async () => {
     const [hashWithoutSalt, salt] = hashed.split('.');
-    assert(hashWithoutSalt);
-    assert(salt);
-    assert.notEqual(password, hashed);
-    assert.notEqual(password, hashWithoutSalt);
-    assert.notEqual(password, salt);
+    expect(hashWithoutSalt).toBeTruthy();
+    expect(salt).toBeTruthy();
+    expect(password).not.toEqual(hashed);
+    expect(password).not.toEqual(hashWithoutSalt);
+    expect(password).not.toEqual(salt);
   });
 
   it('compares given password with hash', async () => {
-    assert(await Password.compare(hashed, password));
-    assert(!(await Password.compare(hashed, 'not-my-password')));
+    expect(await Password.compare(hashed, password)).toBeTruthy();
+    expect(!(await Password.compare(hashed, 'not-my-password'))).toBeTruthy();
   });
 });
