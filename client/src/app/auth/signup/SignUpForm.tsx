6136faa0b704d3cf23992f9750563f2b2c19d718
Berkin Anik <berkinanik@gmail.com>
2024-04-07 05:10:30 +0200

    feat(client): add sign up route and sign up form

---
+++ client/src/app/auth/signup/SignUpForm.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import { type FormEvent } from 'react';
+
+import { Button } from '~/components';
+import { customFetch } from '~/http';
+
+export const SignUpForm = () => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+
+    const response = await customFetch('/api/users/signup', 'POST', {
+      email,
+      password,
+    }).then((res) => {
+      if (res.ok) {
+        console.log('success:', res.data);
+      } else {
+        console.log('errors:', res.errors);
+      }
+    });
+  };
+
+  return (
+    <form className="flex flex-col items-start gap-4" onSubmit={handleSubmit}>
+      <h1 className="text-xl font-medium">Sign Up</h1>
+      <span className="h-[1px] w-full bg-black" />
+      <div className="flex w-[320px] flex-col gap-2">
+        <label htmlFor="email" className="text-sm font-light">
+          Email Address
+        </label>
+        <input
+          className="rounded-md border border-black px-2 py-1"
+          type="text"
+          name="email"
+          autoComplete="email"
+          placeholder="Email"
+          required
+        />
+      </div>
+      <div className="flex w-[320px] flex-col gap-2">
+        <label htmlFor="password" className="text-sm font-light">
+          Password
+        </label>
+        <input
+          className="rounded-md border border-black px-2 py-1"
+          type="password"
+          name="password"
+          autoComplete="new-password"
+          placeholder="Password"
+          required
+        />
+      </div>
+      <Button type="submit">Sign Up</Button>
+    </form>
+  );
+};
