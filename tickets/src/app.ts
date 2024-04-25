34a9d4c45aa2927d592368eda43c59add99e41c3
Berkin Anik <berkinanik@gmail.com>
2024-04-25 13:54:09 +0200

    feat(tickets): add tickets service initials

---
+++ tickets/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { NotFoundError, currentUserMiddleware, errorHandlerMiddleware } from '@b.anik/common';
+
+const app = express();
+
+// Trust incoming traffic coming from proxy
+app.set('trust proxy', true);
+
+// 3rd party middlewares
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+
+// Middlewares run before route handlers
+app.use(currentUserMiddleware);
+
+// Route handlers
+
+// Middlewares after route handlers
+app.all('*', () => {
+  throw new NotFoundError();
+});
+app.use(errorHandlerMiddleware);
+
+export { app };
