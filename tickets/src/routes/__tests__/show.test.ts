e1201953a999f2e25b2b8a66a1ad6501025b9f82
Berkin Anik <berkinanik@gmail.com>
2024-04-26 14:26:45 +0200

    feat(tickets): add tests for list tickets endpoint

--- tickets/src/routes/__tests__/show.test.ts
+++ tickets/src/routes/__tests__/show.test.ts
@@ -26,20 +26,38 @@ describe('show', () => {
       });
   });
 
-  it('should return a 404 if the ticket is not found', async () => {
-    await requestAgent.get(`${apiRoute}/there-is-no-ticket`).send().expect(404);
+  describe('get one ticket', () => {
+    it('should return a 404 if the ticket is not found', async () => {
+      await requestAgent.get(`${apiRoute}/there-is-no-ticket`).send().expect(404);
+    });
+
+    it('should return the ticket if the ticket is found', async () => {
+      const ticket = await requestAgent
+        .get(`${apiRoute}/${existingTicket.id}`)
+        .send()
+        .expect(200)
+        .then((res) => res.body?.ticket as TicketDoc);
+
+      expect(ticket).toBeDefined();
+      expect(ticket.title).toEqual(existingTicket.title);
+      expect(ticket.price).toEqual(existingTicket.price);
+      expect(ticket.userId).toEqual(existingTicket.userId);
+    });
   });
 
-  it('should return the ticket if the ticket is found', async () => {
-    const ticket = await requestAgent
-      .get(`${apiRoute}/${existingTicket.id}`)
-      .send()
-      .expect(200)
-      .then((res) => res.body?.ticket as TicketDoc);
+  describe('get all tickets', () => {
+    it('should return all tickets', async () => {
+      const tickets = await requestAgent
+        .get(apiRoute)
+        .send()
+        .expect(200)
+        .then((res) => res.body?.tickets as TicketDoc[]);
 
-    expect(ticket).toBeDefined();
-    expect(ticket.title).toEqual(existingTicket.title);
-    expect(ticket.price).toEqual(existingTicket.price);
-    expect(ticket.userId).toEqual(existingTicket.userId);
+      expect(tickets).toBeDefined();
+      expect(tickets.length).toEqual(1);
+      expect(tickets[0].title).toEqual(existingTicket.title);
+      expect(tickets[0].price).toEqual(existingTicket.price);
+      expect(tickets[0].userId).toEqual(existingTicket.userId);
+    });
   });
 });
