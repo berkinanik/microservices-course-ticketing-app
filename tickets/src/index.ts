e0ec39299f09efc7de1bfc31434ef0c490bbecf1
Berkin Anik <berkinanik@gmail.com>
2024-04-28 16:42:33 +0200

    feat(tickets): move nats initialization values to env config of tickets-depl

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,8 +12,24 @@ const start = async () => {
     throw new Error('MONGO_URI must be defined');
   }
 
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
   try {
-    await natsWrapper.connect('ticketing', 'tickets', 'http://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!,
+    );
     console.log('Connected to NATS!');
 
     natsWrapper.client.on('close', () => {
