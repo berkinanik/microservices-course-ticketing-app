7ce59024472a017f0601b82b05df75dbf2ffa433
Berkin Anik <berkinanik@gmail.com>
2024-04-29 14:25:03 +0200

    feat(orders): add show single order endpoint

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { Order } from '../models';
-import { requireAuthMiddleware } from '@b.anik/common';
+import { NotAuthorizedError, NotFoundError, requireAuthMiddleware } from '@b.anik/common';
+import { param } from 'express-validator';
+import { isValidObjectId } from 'mongoose';
 
 const router = express.Router();
 
@@ -13,5 +15,32 @@ router.get('/api/orders', requireAuthMiddleware, async (req, res) => {
     orders,
   });
 });
+
+router.get(
+  '/api/orders/:orderId',
+  requireAuthMiddleware,
+  param('orderId')
+    .trim()
+    .notEmpty()
+    .custom((input) => isValidObjectId(input))
+    .withMessage('orderId is required'),
+  async (req, res) => {
+    const { orderId } = req.params as { orderId: string };
+
+    const order = await Order.findById(orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    return res.status(200).send({
+      order,
+    });
+  },
+);
 
 export { router as showOrderRouter };
