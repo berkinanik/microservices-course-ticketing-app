422b6c709673fbaa64780ce56a3b0b7320bc760f
Berkin Anik <berkinanik@gmail.com>
2024-05-02 02:19:33 +0200

    refactor(tickets): make event on message async

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -8,7 +8,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   readonly queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent['data'], _msg?: Message) {
+  async onMessage(data: OrderCreatedEvent['data'], _msg?: Message): Promise<void> {
     return new Promise<void>(async (resolve, reject) => {
       try {
         const ticketId = data.ticket.id;
