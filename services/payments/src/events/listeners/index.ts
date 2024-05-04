03677c5b490cac58b8bce9abfc04e8049dc97736
Berkin Anik <berkinanik@gmail.com>
2024-05-04 02:55:27 +0200

    fix(payments): start order updated listener

--- services/payments/src/events/listeners/index.ts
+++ services/payments/src/events/listeners/index.ts
@@ -1,8 +1,10 @@
 import { Stan } from 'node-nats-streaming';
 import { OrderCreatedListener } from './order-created-listener';
 import { OrderCancelledListener } from './order-cancelled-listener';
+import { OrderUpdatedListener } from './order-updated-listener';
 
 export const startListeners = (client: Stan) => {
   new OrderCreatedListener(client).listen();
   new OrderCancelledListener(client).listen();
+  new OrderUpdatedListener(client).listen();
 };
