a5193162825105cb9542b754a7dc62a9773528ca
Berkin Anik <berkinanik@gmail.com>
2024-05-05 22:43:22 +0200

    feat(client): add orders page

--- apps/client/src/app/orders/page.tsx
+++ apps/client/src/app/orders/page.tsx
@@ -1,16 +1,60 @@
 import { headers } from 'next/headers';
+import Link from 'next/link';
 
 import { type OrdersResponse } from '~/@types';
+import { Button, ExpirationTimer } from '~/components';
+import { OrderStatus } from '~/constants';
 import { buildClientServer } from '~/http';
 
 export default async function OrdersPage() {
   const client = buildClientServer(headers);
   const orders = await client.get<OrdersResponse>(`/api/orders`).then((res) => {
-    if (res.ok) return res.data.orders;
+    if (res.ok) return res.data.orders.toReversed();
 
     return [];
   });
 
-  // TODO orders page
-  return <></>;
+  return (
+    <div>
+      <h1 className="text-lg font-bold">Orders</h1>
+      <div className="flex flex-col items-stretch gap-2">
+        {orders.length ? (
+          orders.map((order) => (
+            <div
+              key={order.id}
+              className="flex flex-row flex-wrap items-end justify-between gap-4 rounded-md border p-4"
+            >
+              <div>
+                <p className="font-semibold">Order ID: {order.id}</p>
+                <Link href={`/tickets/${order.ticket.id}`}>
+                  <p>Ticket ID: {order.ticket.id}</p>
+                </Link>
+                <p>Price: ${order.ticket.price}</p>
+                <p>Status: {order.status}</p>
+              </div>
+
+              {order.status === OrderStatus.AwaitingPayment && (
+                <div className="flex flex-col items-end justify-end gap-2">
+                  {order.expiresAt && <ExpirationTimer expiresAt={order.expiresAt} />}
+
+                  <Link href={`/orders/${order.id}`}>
+                    <Button>Pay now</Button>
+                  </Link>
+                </div>
+              )}
+            </div>
+          ))
+        ) : (
+          <>
+            <p>No orders found.</p>
+            <div>
+              <Link href="/">
+                <Button>Find some tickets</Button>
+              </Link>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
--- apps/client/src/components/navbar/Navbar.tsx
+++ apps/client/src/components/navbar/Navbar.tsx
@@ -12,8 +12,10 @@ export const Navbar: React.FC<Props> = ({ currentUser }) => {
   return (
     <nav className="w-full bg-cyan-700">
       <div className="mx-auto flex max-w-[1200px] flex-row justify-between p-4">
-        <div>
+        <div className="flex flex-row gap-4">
           <Link href="/">Home</Link>
+
+          {currentUser && <Link href="/orders">Orders</Link>}
         </div>
 
         <AuthButtons currentUser={currentUser} />
